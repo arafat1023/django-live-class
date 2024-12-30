d6216da9d0085a6938d74c6c3f42a5ad7e9ba7e9
arafat1023 <arafathossain847@gmail.com>
2024-12-30 15:59:07 +0600

    add html to the blog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 
 def home(request):
-    return HttpResponse("<h1>Welcome to My Blog!</h1>")
+    return render(request, 'blog/home.html')
 
