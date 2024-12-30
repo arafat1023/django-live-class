74e1c4302274134731933e3d31dd75118da297f5
arafat1023 <arafathossain847@gmail.com>
2024-12-30 16:05:36 +0600

    show dynamic content via html

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    recent_posts = [
+        {'title': 'Post 1', 'content': 'Content for post 1'},
+        {'title': 'Post 2', 'content': 'Content for post 2'},
+        {'title': 'Post 3', 'content': 'Content for post 3'},
+    ]
+    return render(request, 'blog/home.html', {'recent_posts': recent_posts})
+
 
